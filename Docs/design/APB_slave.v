6073b188fc771227f7482b362792fd5e2c2f65d6
Meghavs124 <meghavs777@gmail.com>
2026-06-02 12:04:26 +0530

    Modify APB_slave parameters and add error handling
    Updated the APB_slave module parameters and fixed address width. Added PSLVERR logic for error handling.

--- Docs/design/APB_slave.v
+++ Docs/design/APB_slave.v
@@ -1,10 +1,10 @@
-module APB_slave #(parameter depth=256)(
+module APB_slave #(parameter depth=200)(
 	input PCLK,
 	input PRESETn,
 	input PSEL,
 	input PENABLE,
 	input PWRITE,
-	input [8:0]PADDR,
+	input [7:0]PADDR,
 	input [7:0]PWDATA,
 	output reg [7:0]PRDATA,
 	output PREADY
@@ -12,26 +12,41 @@ module APB_slave #(parameter depth=256)(
 	
 reg [7:0] mem[0:depth-1];
 integer i;
+reg PSLVERR;
 
 assign PREADY = 1'b1; 
 
 always @(posedge PCLK or negedge PRESETn)
 begin
 	if(!PRESETn)
+	begin
+		for(i=0; i<=depth-1; i=i+1)
 		begin
-			for(i=0; i<=depth-1; i=i+1)
-			mem[i] <= 8'b0;
+	    	mem[i] <= 8'b0;
+			PRDATA <= 8'b0;
 		end
+	end
     else
-		begin
-			if(PSEL && PENABLE && PWRITE)
-			 mem[PADDR] <= PWDATA;
+	begin
+	   if(PSEL && PENABLE && PWRITE)
+	   mem[PADDR] <= PWDATA;
+	end
+end
+
+always @(*)
+begin
+    if(PSEL &&PENABLE && !PWRITE)
+	 PRDATA = mem[PADDR];
+	 
+	else
+	  PRDATA =0;
 		
-			else if(PSEL &&PENABLE && !PWRITE)
-			 PRDATA <=mem[PADDR];
-			 
-			else
-			 PRDATA<=0;
-		end
+end
+
+always @(*)
+begin
+    PSLVERR=1'b0;
+    if(PRDATA>depth-1)
+    PSLVERR=1'b1;
 end
 endmodule
