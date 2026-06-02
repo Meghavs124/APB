a827ff14766c8888c36af4825a39cf043e50be4f
Meghavs124 <meghavs777@gmail.com>
2026-06-02 12:03:21 +0530

    Refactor APB signals to 8-bit width

--- Docs/design/APB_master.v
+++ Docs/design/APB_master.v
@@ -3,9 +3,9 @@ module APB_master(
 	input PRESETn,
 	input transfer,
 	input READ_WRITE,
-	input [8:0] apb_write_paddr,
+	input [7:0] apb_write_paddr,
 	input [7:0] apb_write_data,
-	input [8:0] apb_read_paddr,
+	input [7:0] apb_read_paddr,
 	input PREADY,
 	input [7:0]PRDATA,
 	output reg PWRITE,
@@ -13,7 +13,7 @@ module APB_master(
 	output reg PSEL2,
 	output reg PENABLE,
 	output reg [7:0]PWDATA,
-	output reg [8:0]PADDR,
+	output reg [7:0]PADDR,
 	output [7:0] apb_read_data_out
 );
 
@@ -88,22 +88,30 @@ begin
     end
 end
 
-always @(*)
+always @(posedge PCLK or negedge PRESETn)
 begin
-	PWRITE = 1'b0;
-    PWDATA = 8'b0;
-    PADDR  = 9'b0;
-	
+if(!PRESETn)
+begin
+	PWRITE <= 1'b0;
+    PWDATA <= 8'b0;
+    PADDR  <= 8'b0;
+end
+	else if(n_state==SETUP)
+	begin
 	if(READ_WRITE)
 		begin
-		PWRITE = 1'b1;
-		PWDATA = apb_write_data;
-        PADDR = apb_write_paddr;
+		PWRITE <= 1'b1;
+		PWDATA <= apb_write_data;
+        PADDR <= apb_write_paddr;
 		end
 	else
 		begin
-		PWRITE = 1'b0;
-		PADDR = apb_read_paddr;
+		PWRITE <= 1'b0;
+		PADDR <= apb_read_paddr;
+		//PRDATA = apb_write_data;
+		//PWDATA = apb_write_data;
+		
+		end
 		end
 end
 
@@ -114,7 +122,7 @@ begin
 	
 	if(c_state == SETUP)
 	begin
-		if (PADDR[8]==0)
+		if (PADDR[7]==0)
 			begin
 			PSEL1=1;
 			PSEL2=0;
@@ -128,7 +136,7 @@ begin
 
 	else if (c_state == ACCESS)
 	begin
-		if (PADDR[8]==0)
+		if (PADDR[7]==0)
 			begin
 			PSEL1 = 1;
 			PSEL2 = 0;
@@ -149,4 +157,4 @@ end
 
 assign apb_read_data_out = PRDATA;
 
-endmodule
+endmodule	
